db98b7b141e4e768d1db426287276b185890f9d3
Jordi Martí <jordimarti@me.com>
2022-01-09 22:13:10 +0100

    Code refactoring for webhooks

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -2,16 +2,16 @@ class WebhooksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    information = request.raw_post
-    result = JSON.parse(information)
-    puts "DATA PARSED"
-    puts result['type']
+    result = JSON.parse(request.raw_post)
 
     case result['type']
     when 'video.asset.ready'
       video = Video.find_by(mux_asset_id: result['object']['id'])
       video.status = result['data']['status']
       video.max_stored_resolution =  result['data']['max_stored_resolution']
+      video.max_stored_frame_rate =  result['data']['max_stored_frame_rate']
+      video.duration = result['data']['duration']
+      video.aspect_ratio = result['data']['aspect_ratio']
       video.save
     end
   end
