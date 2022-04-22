d305e08e9bbdd271e63ec378732d9fd5a044c042
Jordi Martí <jordimarti@me.com>
2022-04-22 21:02:57 +0200

    Solves bug on course_items ordering

--- app/models/course_item.rb
+++ app/models/course_item.rb
@@ -7,6 +7,6 @@ class CourseItem < ApplicationRecord
 
   after_create_commit do
     course = Course.find(self.course_id)
-    broadcast_append_to :course_items, target: "items_#{dom_id(course)}", partial: "course_items/course_item", locals: { course_item: self }
+    broadcast_append_to :course_items, target: "existing_items_#{dom_id(course)}", partial: "course_items/course_item", locals: { course_item: self }
   end
 end
